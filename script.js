ec60cd3508cd619efe6bf82aa05b6480db195dbc
justinochemane <151973399+justinochemane@users.noreply.github.com>
2024-05-08 19:41:43 +0200

    debuging  with 'marked'is not work properly in js script file

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const addBtn = document.getElementById("add");
+const addBtn = document.getElementById('add');
 
-addBtn.addEventListener("click", () => addNewNote("Hello World"));
+addBtn.addEventListener('click', () => addNewNote());
 
-function addNewNote(text = "") {
+function addNewNote(text = '') {
   const note = document.createElement("div");
   note.classList.add("note");
 
   note.innerHTML = `
-      
-
     <div class="tools">
         <button class="edit"><i class="fas fa-edit"></i></button>
         <button class="delete"><i class="fas fa-trash-alt"></i></button>
@@ -24,7 +22,7 @@ function addNewNote(text = "") {
   const textArea = note.querySelector("textarea");
 
   textArea.value = text;
-  main.innerHTML = marked(text);
+  main.innerHTML = marked(text); 
 
   deleteBtn.addEventListener("click", () => {
     note.remove();
